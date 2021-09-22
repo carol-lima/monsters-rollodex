7433900edfb55b743cc1518ec1577eafd2d186b0
Carol <clima@cobasi.com.br>
2021-09-22 20:00:32 -0300

    Card list component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
+import { CardList } from './components/card-list/card-list.component';
+
 function App() {
-  const [monsters, setMonsters] = useState([])
+  const [monsters, setMonsters] = useState([]);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => setMonsters(users))
-  }, [])
+    .then(users => setMonsters(users));
+  }, []);
 
   return (
     <div className="App">
-      {monsters.map(monster => (
-        <h1 key={monster.id}>{monster.name}</h1>
-      ))}
+      <CardList>
+        {monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </CardList>
+      
     </div>
   );
 }
