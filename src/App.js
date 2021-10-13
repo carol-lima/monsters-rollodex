c24b1cad27178e6d736f4ef90d9a812dc18a763d
Carol <clima@cobasi.com.br>
2021-10-13 08:20:01 -0300

    Filtering state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { CardList } from './components/card-list/card-list.component';
 
 function App() {
   const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -12,9 +13,14 @@ function App() {
     .then(users => setMonsters(users));
   }, []);
 
+  const filteredMonsters = monsters.filter(monster => 
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <CardList monsters={monsters} />
+      <input type='search' placeholder='search monsters' onChange={e => setSearchField(e.target.value)} />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
