2a7bf261e0629a0ed3aa53da2399948d91a0f257
carol-lima <carollima.dev@gmail.com>
2021-11-26 23:03:37 -0300

    Finish layout, font and search

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 function App() {
   const [monsters, setMonsters] = useState([]);
@@ -17,9 +18,17 @@ function App() {
     monster.name.toLowerCase().includes(searchField.toLowerCase())
   );
 
+  const handleChange = e => {
+    setSearchField(e)
+  }
+
   return (
     <div className="App">
-      <input type='search' placeholder='search monsters' onChange={e => setSearchField(e.target.value)} />
+      <h1> Monsters Rolodex </h1>
+      <SearchBox
+        placeholder='search monsters'
+        handleChange={e => handleChange(e.target.value)}
+      />
       <CardList monsters={filteredMonsters} />
     </div>
   );
